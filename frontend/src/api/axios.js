4953c6f46fbd8a78365cbd73df9e920d0bc8faee
zanemaze18-max <zanemaze18@gmail.com>
2026-04-18 21:14:52 +0800

    Update API base URL to use environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://thefolio-of34.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((config) => {
